72365aaee1c0278db342e18843e9c9b8eb867a58
Raphael Motta <94933819+RMotta-afk@users.noreply.github.com>
2025-03-21 18:34:49 -0300

    frst-commit;project-setup

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Env Easy</h1>
+            <p>Creating envs for devs</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/renderer/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+describe('TESTES', () => {
+    it('Descricao (Itumba)', () => {
+        var sung = 1;
+        expect(sung).toBe(1);
+    });
+})
+
